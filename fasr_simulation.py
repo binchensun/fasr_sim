ccbb08ca79c5c03c568b4a5da4d4e9dd7499b9a4
Bin Chen <bin.chen@njit.edu>
2021-01-26 07:27:43 -0500

    minor update of KSC site file

--- fasr_simulation.py
+++ fasr_simulation.py
@@ -91,8 +91,9 @@ def ksc_site2cfg(sitefile='sites/KSC_Antenna_Sites.txt', ants=[0, 1, 2, 3, 4, 5,
                              blockagediam='0.75m', maxrad='1.784deg',
                              reffreq='1.0GHz', dopb=True)
 
+    obstable = os.getenv("CASAPATH").split(' ')[0] + "/data/geodetic/Observatories"
+    tb.open(obstable, nomodify=True)
     if 'KSC' not in tb.getcol('Name'):
-        obstable = os.getenv("CASAPATH").split(' ')[0] + "/data/geodetic/Observatories"
         obsdict_dsc = {'MJD': 59147.0, 'Name': 'KSC', 'Type': 'WGS84', 'Long': -80.693, 'Lat': 28.508, 'Height': 3.00,
                        'X': 0.0, 'Y': 0.0, 'Z': 0.0, 'Source': 'EOVSA Team'}
         tb.open(obstable, nomodify=False)
